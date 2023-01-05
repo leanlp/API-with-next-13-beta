031cc55936d7734840c226d491dd20e1d73a048d
leandro <0xleanlp@gmail.com>
2023-01-04 21:25:17 -0300

    v1

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import { Navigation } from "./components/Navigation";
+
+export default function RootLayout({ children }) {
+
+  return (
+    <html>
+     <head/>
+      <body>
+       <Navigation/>
+        {children}
+      </body>
+    </html>
+  )
+}
