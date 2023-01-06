9c1e40c7c28873394e7ab43f8cfc36d5b409bb0c
leandro <0xleanlp@gmail.com>
2023-01-06 15:33:02 -0300

    font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,19 @@
 import "../styles/globals.css"
 import { Navigation } from "./components/Navigation";
+import { Inter, Roboto, Space_Grotesk } from '@next/font/google';
 
+const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: '700',
+
+  subsets: ['latin'],
+})
+const font = Space_Grotesk({weight: "500"})
 
 export default function RootLayout({ children }) {
 
   return (
-    <html>
+    <html lang="en" className={font.className}>
      <head/>
       <body>
        <Navigation/>
